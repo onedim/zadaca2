bf01b24fea263438b4daa86df14a00b89f088f51
onedim <o.nedim@gmail.com>
2014-11-19 11:42:35 +0100

    zavrsen

--- Jaja_podjela_na_tuce.java
+++ Jaja_podjela_na_tuce.java
@@ -1,9 +1,18 @@
+import java.util.Scanner;
+
 
 public class Jaja_podjela_na_tuce {
 
 	public static void main(String[] args) {
-		
-
+		Scanner unos=new Scanner(System.in);
+		System.out.println("Unesi broj jaja");
+		int brJaja=unos.nextInt();
+		int korpaJaja=brJaja/144;
+		int tuceJaja=(brJaja%144)/12;
+		int ostatJaja=(brJaja%144)%12;
+		System.out.println(korpaJaja);
+		System.out.println(tuceJaja);
+		System.out.println(ostatJaja);
 	}
 
 }
